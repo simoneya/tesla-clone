b0b0a0ad03bc54a9005df995a4143156bf0735f1
simoneya <yavar.simona@yahoo.com>
2022-12-05 15:33:44 +0000

    update

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from "styled-components";
 
-function Section() {
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
        <ItemText>
-           <h1>Model S</h1>
-           <p>Order Online for Touchless Delivery</p>
+           <h1> { title } </h1>
+           <p> { description } </p>
        </ItemText>
        <Buttons>
             <ButtonGroup>
                 <LeftButton>
-                        Custom Order
+                        { leftBtnText }
                 </LeftButton>
                 <RightButton>
-                        Existing Inventory
+                        { rightBtnText }
                 </RightButton>
             </ButtonGroup>
                 <DownArrow src="/images/down-arrow.svg"/>
@@ -36,6 +36,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content: space-between; /*vertical alignment*/
     align-items: center; /*horizontal alignment*/
+    background-image: ${props => `url("/images/${props.bgImage}")`}
 `;
 
 const ItemText = styled.div`
@@ -46,6 +47,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
+     @media (max-width: 768px) {
+        flex-direction: column;
+     }
 `;
 
 const LeftButton = styled.div`
