5295fbcf93b930295acc944e90ea04dad4b078ee
simoneya <yavar.simona@yahoo.com>
2022-12-06 15:54:58 +0000

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,6 +30,7 @@ const Container = styled.div`
     position: fixed;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 0 20px;
     top: 0;
     left: 0;
@@ -47,6 +48,10 @@ const Menu = styled.div`
             text-transform: uppercase;
             padding: 0 10px;
             flex-wrap: nowrap;
+        }
+
+        @media (max-width: 768px) {
+            display: none;
         }
 `;
 
