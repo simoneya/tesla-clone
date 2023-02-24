782aebfa92dd69fc365fae9bf88592936e77c291
simoneya <yavar.simona@yahoo.com>
2023-02-24 21:02:49 +0000

    add firebase files

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD5f7mwNARKhAa3BDMDErUBUybz0zJ1bX8",
+    authDomain: "tesla-clone-1509f.firebaseapp.com",
+    projectId: "tesla-clone-1509f",
+    storageBucket: "tesla-clone-1509f.appspot.com",
+    messagingSenderId: "547692566507",
+    appId: "1:547692566507:web:da2af78d3dc77afc01ebae",
+    measurementId: "G-11H85Q32C8"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
